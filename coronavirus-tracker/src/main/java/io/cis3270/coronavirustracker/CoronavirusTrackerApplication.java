f6b0a33e0d74c4bbdb370fe338000cd581d6c28f
yomar3270 <60445638+yomar3270@users.noreply.github.com>
2020-09-25 15:08:38 -0400

    Add files via upload

---
+++ coronavirus-tracker/src/main/java/io/cis3270/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,18 @@
+package io.cis3270.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+		System.out.println("locationStats");
+
+
+	}
+
+}
---
+++ coronavirus-tracker/src/test/java/io/cis3270/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cis3270.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
